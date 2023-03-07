e3888b1fce5bb2f58c541584655f6f32481a1475
Andrew Bastin <andrewbastin.k@gmail.com>
2023-03-07 11:52:36 +0530

    chore: add dts generation

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import * as path from "path"
 
 import vue from '@vitejs/plugin-vue'
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,5 +24,6 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    dts()
   ],
 })
